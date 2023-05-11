4e755ccdf3df49dc13c442f225d8856f7ca6430e
Adi Astawa <adiast457@gmail.com>
2023-05-11 17:29:32 +0800

    adjust some content

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -2,25 +2,19 @@ $(document).ready(function() {
   // click event listeners on navbar links
   const fileLocation = "src/components/content/"
   $("#content").load(fileLocation+"profile.html");
-$("#profile-link").click(function() {
-    $("#content").load(fileLocation+"profile.html");
-});
-$("#about-link").click(function() {
-  $("#content").load(fileLocation+"profile.html");
-  history.pushState(null, null, '#about');
-});
+  $("#profile-link").click(function() {
+      $("#content").load(fileLocation+"profile.html");
+  });
 
-$("#skill-link").click(function() {
-  $("#content").load(fileLocation+"profile.html");
-  history.pushState(null, null, '#skill');
-});
+  $("#skill-link").click(function() {
+    $("#content").load(fileLocation+"skill.html");
+  });
 
-$("#contact-link").click(function() {
-  $("#content").load(fileLocation+"profile.html");
-  history.pushState(null, null, '#contact');
-});
+  $("#contact-link").click(function() {
+    $("#content").load(fileLocation+"contact.html");
+  });
 
-$("#projects-link").click(function() {
-    $("#content").load(fileLocation+"projects.html");
-});
+  $("#projects-link").click(function() {
+      $("#content").load(fileLocation+"projects.html");
+  });
 });
